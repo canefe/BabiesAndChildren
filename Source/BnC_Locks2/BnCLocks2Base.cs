388882fbb442c48feccab7151d5a8325f1dda390
canefe <turkiyecraft@gmail.com>
2021-08-17 00:51:25 +0300

    some stuff

---
+++ Source/BnC_Locks2/BnCLocks2Base.cs
@@ -0,0 +1,28 @@
+﻿using HugsLib;
+using RimWorld;
+using System.Runtime.Remoting.Messaging;
+using Verse;
+
+namespace BabiesAndChildren
+{
+    public class BnCLocks2 : ModBase
+    {
+        public static BnCLocks2 Instance { get; private set; }
+        
+        public override string ModIdentifier => "Babies_and_Children Locks2 Patch";
+
+        private BnCLocks2()
+        {
+            Instance = this;
+        }
+
+        public override void DefsLoaded()
+        {
+            Log.Message("BnC_Locks2_Patch");
+
+        }
+
+    }
+
+
+}
---
+++ Source/BnC_Locks2/LockConfigToddlers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Verse;
+using BabiesAndChildren.api;
+using Locks2.Core;
+
+namespace BabiesAndChildren.Locks2
+{
+    public partial class LockConfigBase : LockConfig
+    {
+        public class LockConfigToddlers : IConfigRule
+        {
+            public bool enabled;
+
+            public override float Height => 54;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public override bool Allows(Pawn pawn)
+            {
+                if (enabled && AgeStages.IsYoungerThan(pawn, AgeStages.Child)) return true;
+                return false;
+            }
+
+            public override void DoContent(IEnumerable<Pawn> pawns, Rect rect, Action notifySelectionBegan,
+                Action notifySelectionEnded)
+            {
+                var before = enabled;
+                Widgets.CheckboxLabeled(rect, "Allow toddlers/babies", ref enabled);
+                if (before != enabled) Notify_Dirty();
+            }
+
+            public override IConfigRule Duplicate()
+            {
+                return new ConfigRulePrisoners { enabled = enabled };
+            }
+
+            public override void ExposeData()
+            {
+                Scribe_Values.Look(ref enabled, "enabled", true);
+            }
+        }
+    }
+    
+}
